95ca792aecc7dabcc0d56d157de184185918cd84
sieradzki <sieradzki.mateusz@outlook.com>
2021-12-02 19:52:24 +0100

    part 2

--- Day_2/position.py
+++ Day_2/position.py
@@ -3,18 +3,21 @@ with open("input.txt", "r") as f:
 
 horizontal = 0
 depth = 0
+aim = 0
 
 for line in data:
     line = line.strip()
     if line.find("forward") != -1:
         horizontal += int(line[-1])
+        depth += aim * int(line[-1])
     elif line.find("down") != -1:
-        depth += int(line[-1])
+        aim += int(line[-1])
     elif line.find("up") != -1:
-        depth -= int(line[-1])
+        aim -= int(line[-1])
 
 print(f"Horizontal: {horizontal}")
 print(f"Depth: {depth}")
+print(f"Aim: {aim}")
 
 final = horizontal * depth
 
